ef09fad7ab255712c701250cabbd7480dc10e6cc
Srinivasa465 <srinivasasenthil.thirugnanam@trumpf.com>
2026-03-05 10:03:51 +0530

    Firstcode commited

---
+++ firstcode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Solution {
+   public static void Main (String[] args) {
+
+
+      Console.WriteLine ("Enter the name");
+      String str = Console.ReadLine ();
+
+      String reversed = "";
+
+      for (int i = str.Length - 1; i >= 0; i--) {
+         reversed += str[i];
+      }
+
+      Console.WriteLine ("Reversed string : " + reversed);
+   }
+
+
+}
